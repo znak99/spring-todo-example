7cf50b5544abac4620ef6a5f440fccc9696d35bb
znak99 <xhahekcldhkznak0113@gmail.com>
2024-06-08 13:38:44 +0900

    Mark up index.html page

---
+++ src/main/java/io/github/znak99/spring_todo_example/SpringTodoExampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.znak99.spring_todo_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/znak99/spring_todo_example/SpringTodoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.znak99.spring_todo_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
